f37b598ed138fdaca026beefa4fcdc515545f883
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-08-05 23:34:09 -0400

    added navigation an redux storage

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "@components": "./src/components",
+          "@routes": "./src/routes",
+          "@screens": "./src/screens",
+          "@store": "./src/store",
+          "@interfaces": "./src/interfaces",
+          "@utils": "./src/utils",
+        }
+      }
+    ]
+  ]
 };
---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const Welcome: FC = () => {
+  return (
+    <View>
+      <Text>Hola soy Welcome</Text>
+    </View>
+  )
+}
+
+export default Welcome
