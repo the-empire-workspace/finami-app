ac33cdf270bf83f6e8a278f85442d309d64de43f
Jorge Bastidas <jorgebastidas9@gmail.com>
2023-12-13 23:58:44 -0400

    remove deleted

--- src/screens/Goals/elements/index.ts
+++ src/screens/Goals/elements/index.ts
@@ -1,5 +1,3 @@
-export {default as FixedOutcome} from './FixedOutcome'
-export {default as PendingOutcome} from './PendingOutcome'
 export {default as MainGoals} from './MainGoals'
 export {default as NewGoals} from './NewGoals'
 export {default as DetailGoals} from './DetailGoals'
